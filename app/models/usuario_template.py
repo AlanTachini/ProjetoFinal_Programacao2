f4c40e64c0c8f038903800e0fb774a4fab44c713
MateusTama <mateustamasia3@gmail.com>
2025-11-02 18:26:16 -0300

    Correção UsuarioPlano e UsuarioTemplate.

--- app/models/usuario_template.py
+++ app/models/usuario_template.py
@@ -1,8 +1,5 @@
-class UsuarioPlano:
-    def __init__(self, id=None, id_usuario=None, id_plano=None, valor_pago=None, data_aquisicao=None, data_expiracao=None):
+class UsuarioTemplate:
+    def __init__(self, id=None, id_usuario=None, id_template=None):
         self.id = id
         self.id_usuario = id_usuario
-        self.id_plano = id_plano
-        self.valor_pago = valor_pago
-        self.data_aquisicao = data_aquisicao
-        self.data_expiracao = data_expiracao
+        self.id_template = id_template
